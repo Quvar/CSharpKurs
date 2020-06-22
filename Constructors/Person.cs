b4d0e2c7813d8a26c3c8631939c19f1d706fe9e3
quvar <quvar123@gmail.com>
2020-06-23 00:56:46 +0200

    bruh

---
+++ Constructors/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Person
+    {
+        int id;
+        int age;
+        string firstname;
+        string lastname;
+        string gender;
+        string address;
+
+        public Person()
+        {
+            id = 1;
+            age = 20;
+            firstname = "Filip";
+            lastname = "Dulny";
+            gender = "male";
+        }
+        public Person(int id, int age, string firstname, string lastname, string gender, string address)
+        {
+            this.id = id;
+            this.age = age;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.gender = gender;
+            this.address = address;
+        }
+        public Person(int id, int age, string firstname, string lastname)
+        {
+            this.id = id;
+            this.age = age;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            address = "Stalowa Wola";
+        }
+        public Person(int id, int age)
+        {
+            this.id = id;
+            this.age = age;
+        }
+
+    }
+}
--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,14 +6,69 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            //string name = args[0];
+            ValueType();
 
-            //Console.WriteLine("Witaj " + name);
-
-            foreach (var item in args)
+            for (; ; )
             {
-                Console.WriteLine("Witaj, " + item);
+                Greeting();
+                Age();
+                Settings();
             }
+        }
+
+        private static void ValueType()
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            long maxLong = long.MaxValue;
+            long minLong = long.MinValue;
+
+            Console.WriteLine("maxInt = " + maxInt);
+            Console.WriteLine("minInt = " + minInt);
+            Console.WriteLine("maxLong = " + maxLong);
+            Console.WriteLine("minLong = " + minLong);
+        }
+
+        /// <summary>
+        /// Ustawienia
+        /// </summary>
+        private static void Settings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
+        /// <summary>
+        /// Wypisujemy komunikat zależny od wieku użytkownika
+        /// </summary>
+        private static void Age()
+        {
+            Console.Write("Ile masz lat? ");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+            if (age > 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Brawo, jesteś pełnoletni i możesz napić się piwa");
+            }
+            else if (result == false)
+            {
+                Console.WriteLine("Wprowadziłeś nieprawidłowy wiek!!");
+            }
+            else
+            {
+                Console.WriteLine("Przykro nam, możemy zaoferować ci tylko mleko");
+            }
+        }
+        /// <summary>
+        /// Witamy użytkownika
+        /// </summary>
+        private static void Greeting()
+        {
+            Console.Write("Podaj swoje imię: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Witaj " + name);
         }
     }
 }
